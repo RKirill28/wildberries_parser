6b55536fce119cb84296d70fa87941eb022a897b
Kirill <46402679+RKirill28@users.noreply.github.com>
2025-07-04 12:21:11 +0700

    Update parser.py
    Отрефачил выбор баскета в __get_images

--- parser.py
+++ parser.py
@@ -65,42 +65,20 @@ class ParseWB:
 
     @staticmethod
     def __get_images(item_model: Items):
+        basket_ranges = [
+            (143, '01'), (287, '02'), (431, '03'), (719, '04'), (1007, '05'),
+            (1061, '06'), (1115, '07'), (1169, '08'), (1313, '09'), (1601, '10'),
+            (1655, '11'), (1919, '12'), (2045, '13'), (2189, '14'), (2405, '15')
+        ]
+
         for product in item_model.products:
             _short_id = product.id // 100000
-            """Используем match/case для определения basket на основе _short_id"""
-            if 0 <= _short_id <= 143:
-                basket = '01'
-            elif 144 <= _short_id <= 287:
-                basket = '02'
-            elif 288 <= _short_id <= 431:
-                basket = '03'
-            elif 432 <= _short_id <= 719:
-                basket = '04'
-            elif 720 <= _short_id <= 1007:
-                basket = '05'
-            elif 1008 <= _short_id <= 1061:
-                basket = '06'
-            elif 1062 <= _short_id <= 1115:
-                basket = '07'
-            elif 1116 <= _short_id <= 1169:
-                basket = '08'
-            elif 1170 <= _short_id <= 1313:
-                basket = '09'
-            elif 1314 <= _short_id <= 1601:
-                basket = '10'
-            elif 1602 <= _short_id <= 1655:
-                basket = '11'
-            elif 1656 <= _short_id <= 1919:
-                basket = '12'
-            elif 1920 <= _short_id <= 2045:
-                basket = '13'
-            elif 2046 <= _short_id <= 2189:
-                basket = '14'
-            elif 2190 <= _short_id <= 2405:
-                basket = '15'
-            else:
-                basket = '16'
-
+            basket = '16'
+            for upper_bound, basket_value in basket_ranges:
+                if _short_id <= upper_bound:
+                    basket = basket_value
+                    break
+                 
             """Делаем список всех ссылок на изображения и переводим в строку"""
             link_str = "".join([
                 f"https://basket-{basket}.wb.ru/vol{_short_id}/part{product.id // 1000}/{product.id}/images/big/{i}.jpg;"
